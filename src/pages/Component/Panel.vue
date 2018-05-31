9d18b03b763a0276a835edbf9589d0c7a487da1b
tui95 <khemmatat.thean@gmail.com>
2018-05-31 21:39:01 +0700

    change button to link

--- src/pages/Component/Panel.vue
+++ src/pages/Component/Panel.vue
@@ -1,11 +1,17 @@
 <template>
     <f7-page>
         <f7-navbar>
-            <f7-button big href="/profile" icon-material="account_circle" icon-size="40px" view="#main-view" panel-close> Example</f7-button>
+            <f7-button big href="/profile/" icon-material="account_circle" icon-size="40px" view="#main-view" panel-close> Example</f7-button>
         </f7-navbar>
         <f7-list>
-            <f7-list-item v-for="item in panelItems" :key="item.title">
-                <f7-button :href="item.path" :icon-material="item.icon" view="#main-view" panel-close> {{item.title}}</f7-button>
+            <f7-list-item v-for="item in panelItems" :key="item.title"
+                          :link="item.path"
+                          view="#main-view"
+                          panel-close>
+                <div>
+                    <f7-icon :material="item.icon"></f7-icon>
+                    {{item.title}}
+                </div>
             </f7-list-item>
         </f7-list>
         <f7-button icon-material="exit_to_app">Sign out</f7-button>
@@ -13,14 +19,16 @@
 </template>
 
 <script>
+    import F7Icon from "framework7-vue/src/components/icon";
     export default {
+        components: {F7Icon},
         computed: {
             panelItems() {
                 return [
-                    {title: "My schedule", path:"/my-schedule", icon: "today"},
-                    {title: "My group", path:"/my-group", icon: "people"},
-                    {title: "Create group", path:"/create-group", icon: "add_circle"},
-                    {title: "Enroll in a group", path:"/enroll-group", icon: "group_add"},
+                    {title: "My schedule", path:"/my-schedule/", icon: "today"},
+                    {title: "My group", path:"/my-group/", icon: "people"},
+                    {title: "Create group", path:"/create-group/", icon: "add_circle"},
+                    {title: "Enroll in a group", path:"/enroll-group/", icon: "group_add"},
                 ]
             }
         }
