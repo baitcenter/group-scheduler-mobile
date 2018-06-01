23381f1085f1450c968036573e5a300c1f732762
tui95 <khemmatat.thean@gmail.com>
2018-06-02 00:22:39 +0700

    add basic login

--- src/pages/Authentication/Login.vue
+++ src/pages/Authentication/Login.vue
@@ -3,22 +3,39 @@
         <f7-login-screen-title>Login</f7-login-screen-title>
         <f7-list form>
             <f7-list-item>
-                <f7-label>Username</f7-label>
-                <f7-input name="username" placeholder="Username" type="text"></f7-input>
+                <f7-label>Email</f7-label>
+                <f7-input placeholder="Email" type="email"
+                          @input="email = $event.target.value"
+                          required validate clear-button>
+                </f7-input>
             </f7-list-item>
             <f7-list-item>
                 <f7-label>Password</f7-label>
-                <f7-input name="password" type="password" placeholder="Password"></f7-input>
+                <f7-input type="Password" placeholder="Password"
+                          @input="password  = $event.target.value"
+                          required validate clear-button>
+                </f7-input>
             </f7-list-item>
         </f7-list>
         <f7-list>
-            <f7-list-button title="Sign In" login-screen-close></f7-list-button>
+            <f7-list-button title="Sign In" @click="userSignIn"></f7-list-button>
         </f7-list>
     </f7-page>
 </template>
 
 <script>
     export default {
+        data() {
+            return {
+                email: '',
+                password: '',
+            }
+        },
+        methods: {
+            userSignIn() {
+                this.$store.dispatch('userSignIn', {email: this.email, password: this.password})
+            }
+        }
     }
 </script>
 
