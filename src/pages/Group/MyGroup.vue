b42c29526ce6c1794f6c2614afd4a9f3aeea131e
tui95 <khemmatat.thean@gmail.com>
2018-06-04 20:23:22 +0700

    implement displaying userGroups from db

--- src/pages/Group/MyGroup.vue
+++ src/pages/Group/MyGroup.vue
@@ -2,15 +2,15 @@
     <f7-page>
         <f7-navbar title="My Group" back-link="Back"></f7-navbar>
         <f7-list>
-            <f7-card v-for="group in myGroups" :key="group.id"
+            <f7-card v-for="group in myGroups" :key="group.groupId"
                      :title="group.groupName">
                 <f7-card-footer>
                     <span></span>
-                    <f7-link @click="redirectTo(group.id)">See more</f7-link>
+                    <f7-link @click="redirectTo(group.groupId)">See more</f7-link>
                 </f7-card-footer>
             </f7-card>
         </f7-list>
-        {{myGroups}}
+        <!--{{myGroups}}-->
         <!--{{groups}}-->
     </f7-page>
 </template>
@@ -20,12 +20,28 @@
     export default {
         data() {
             return {
+                myGroups: null
             }
         },
         methods: {
             redirectTo(groupId) {
                 this.$f7router.navigate('/group/'+groupId+'/')
+            },
+            populateMyGroup() {
+                let groups = []
+                this.$firebaseRefs.groups.once('value', snapshot => {
+                    for (let groupKey in snapshot.val()) {
+                        if (this.myGroupsKey.includes(groupKey)) {
+                            // console.log(groupKey)
+                            let group = {...snapshot.val()[groupKey]}
+                            group["groupId"] = groupKey
+                            groups.push(group)
+                        }
+                    }
+                })
+                this.myGroups = groups
             }
+
         },
         firebase() {
             return {
@@ -39,28 +55,14 @@
                 this.$firebaseRefs.userGroups.once('value', snapshot => {
                     snapshot.forEach(group => {
                         keys.push(group.key)
-                        // console.log(group.key)
                     })
                 })
-
                 return keys
-
-                // return [
-                //     {name: "Group 1", id: "jnvlnv"},
-                //     {name: "Group 2", id: "fkv"}
-                // ]
             },
-            myGroups() {
-                let groups = []
-                this.$firebaseRefs.groups.once('value', snapshot => {
-                    snapshot.forEach(group => {
-                        groups.push(group.val())
-                        console.log(group.val())
-                    })
-                })
-                return groups
-            }
         },
+        mounted() {
+            this.populateMyGroup()
+        }
     }
 </script>
 
