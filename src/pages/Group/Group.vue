43df5de698dbbe403978c5755b7053d53c977c6a
DESKTOP-3JDRVSO\User <chaninac127@gmail.com>
2018-06-05 20:01:36 +0700

    add in copy-text-to-clipboard(Group page)

--- src/pages/Group/Group.vue
+++ src/pages/Group/Group.vue
@@ -12,7 +12,8 @@
                     </f7-list-item>
                 </f7-accordion-content>
             </f7-list-item>
-            <f7-list-item>Invite code: {{inviteCode}}</f7-list-item>
+            <f7-list-item>Invite code: <div id="invite_code">{{inviteCode}}</div></f7-list-item>
+            <f7-button @click="copyToClipboard">Copy to clipboard</f7-button>
         </f7-list>
         <f7-block>
             <f7-button color="red" class="col-80" fill @click="redirectTo">Schedule</f7-button>
@@ -24,6 +25,7 @@
                 <p>This is the group page. You can invite people into the group using by giving them the group's "Invite code" which they can in turn use it in the Enroll page's form.</p>
             </f7-block>
         </f7-popover>
+        <f7-input type="hidden" id="testing-code" v-bind:value="inviteCode"></f7-input>
     </f7-page>
 </template>
 
@@ -43,6 +45,25 @@
         },
         // user $f7route.params to get the params from url
         methods: {
+            copyToClipboard(){
+                const app = this.$f7
+                var copyText = this.inviteCode
+                console.log(copyText)
+                let testingCopy = document.querySelector('#testing-code')
+                testingCopy.setAttribute('type','text')
+                testingCopy.select()
+                try{
+                    var success = document.execCommand("copy")
+                    var msg = success ? 'successful' : "failed"
+                    app.dialog.alert("copying " + msg)
+                }catch(error){
+                    app.dialog.alert("failed copying the text")
+                }
+                testingCopy.setAttribute('type','hidden')
+                window.getSelection().removeAllRanges()
+                console.log("copy process ended")
+                // document.execCommand("copy")
+            },
             redirectTo() {
                 const groupId = this.$f7route.params.groupId
                 this.$f7router.navigate("/group/"+groupId+"/schedule/")
