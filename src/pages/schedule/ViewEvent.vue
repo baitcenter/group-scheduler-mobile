1c5ab2589c7f544e1c4442c1e7ad4c4822aa3a8e
DESKTOP-4NB3TBD\Bright <bright40081@gmail.com>
2018-06-06 18:25:13 +0700

    change add navigate

--- src/pages/schedule/ViewEvent.vue
+++ src/pages/schedule/ViewEvent.vue
@@ -227,7 +227,7 @@ export default {
                 this.joinEvent()
                 app.dialog.alert('You joined the event!')
                 console.log(groupId,eventId)
-                this.$f7router.navigate('/group/'+groupId+'/schedule/')
+                this.$f7router.navigate('/group/'+groupId+'/')
             });
         },
         openConfirmLeave(){
