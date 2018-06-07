993ce03059ad6edea8dcb7a80681e9c919d97a21
tui95 <khemmatat.thean@gmail.com>
2018-06-07 13:54:33 +0700

    add preloader

--- src/pages/Group/MyGroup.vue
+++ src/pages/Group/MyGroup.vue
@@ -1,20 +1,22 @@
 <template>
     <f7-page>
-        <f7-navbar color="red" title="My Group" back-link="Back">
-          <f7-nav-right><f7-link href="/home/" icon-if-md="material:home"></f7-link></f7-nav-right>
-        </f7-navbar>
-        <f7-list v-if="myGroups.length !== 0">
-            <f7-card v-for="group in myGroups" :key="group.groupId"
-                     :title="group.groupName">
-                <f7-card-footer>
-                    <span></span>
-                    <f7-link @click="redirectTo(group.groupId)">See more</f7-link>
-                </f7-card-footer>
-            </f7-card>
-        </f7-list>
-        <f7-block v-else strong>
-            <p>You haven't enrolled in any group yet.</p>
-        </f7-block>
+        <f7-page-content v-if="loaded">
+            <f7-navbar color="red" title="My Group" back-link="Back">
+                <f7-nav-right><f7-link href="/home/" icon-if-md="material:home"></f7-link></f7-nav-right>
+            </f7-navbar>
+            <f7-list v-if="myGroups.length !== 0">
+                <f7-card v-for="group in myGroups" :key="group.groupId"
+                         :title="group.groupName">
+                    <f7-card-footer>
+                        <span></span>
+                        <f7-link @click="redirectTo(group.groupId)">See more</f7-link>
+                    </f7-card-footer>
+                </f7-card>
+            </f7-list>
+            <f7-block v-else strong>
+                <p>You haven't enrolled in any group yet.</p>
+            </f7-block>
+        </f7-page-content>
     </f7-page>
 </template>
 
@@ -23,7 +25,8 @@
     export default {
         data() {
             return {
-                myGroups: []
+                myGroups: [],
+                loaded: false
             }
         },
         methods: {
@@ -31,6 +34,7 @@
                 this.$f7router.navigate('/group/'+groupId+'/')
             },
             populateMyGroups() {
+                this.$f7.dialog.preloader('Loading');
                 let groups = []
                 this.$firebaseRefs.groups.once('value', snapshot => {
                     for (let groupId in snapshot.val()) {
@@ -40,8 +44,11 @@
                             groups.push(group)
                         }
                     }
+                }).then(() => {
+                    this.myGroups = groups
+                    this.loaded = true
+                    this.$f7.dialog.close()
                 })
-                this.myGroups = groups
             }
 
         },
@@ -63,10 +70,7 @@
             },
         },
         created() {
-            const app = this.$f7
-            app.dialog.preloader('Loading')
             this.populateMyGroups()
-            app.dialog.close()
         }
     }
 </script>
