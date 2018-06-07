a36e989c4a0d8b968a81a3bb6bedd1c38cffc529
tui95 <khemmatat.thean@gmail.com>
2018-06-07 13:26:17 +0700

    fix display value of undefined (groupLeader.name)

--- src/pages/Group/Group.vue
+++ src/pages/Group/Group.vue
@@ -1,45 +1,48 @@
 <template>
     <f7-page>
-        <f7-navbar color="red" :title="groupData.groupName" back-link="Back">
-            <f7-nav-right>
-              <f7-link icon-if-md="material:info" class="popover-open" href="#" data-popover=".popover-info">
-                </f7-link><f7-link href="/home/" icon-if-md="material:home"></f7-link>
-            </f7-nav-right>
-        </f7-navbar>
-        <f7-list>
-            <f7-list-item>Group Name: {{groupData.groupName}}</f7-list-item>
-            <f7-list-item>Description: {{groupData.groupDescription}}</f7-list-item>
-            <f7-list-item media-item>Leader:
-                <f7-chip :text="groupData.groupLeader.name" media-bg-color="orange">
-                  <f7-icon slot="media" material="person"></f7-icon>
-                </f7-chip>
-            </f7-list-item>
-            <f7-list-item accordion-item title="Members">
-                <f7-accordion-content>
-                    <f7-list-item v-for="(member, index) in groupData.groupMembers" :key="index">
-                        <f7-chip :text="member.name" media-bg-color="orange">
-                          <f7-icon slot="media" material="person"></f7-icon>
-                        </f7-chip>
-                    </f7-list-item>
-                </f7-accordion-content>
-            </f7-list-item>
-            <f7-list-item title="Invite code:">
-                <f7-badge style="font-size: 14px">{{groupId}}</f7-badge>
-                <f7-button @click="copyToClipboard" icon-material="content_copy"></f7-button>
-            </f7-list-item>
-        </f7-list>
-        <f7-block>
-            <f7-button color="red" class="col-80" fill @click="redirectTo">Schedule</f7-button>
-            <f7-button color="red" class="col-80" fill @click="openConfirmLeave(uid)">Leave Group</f7-button>
-            <f7-button color="red" class="col-80" fill @click="openConfirmDelete">Delete Group</f7-button>
-        </f7-block>
-        <f7-popover class="popover-info">
+        <f7-page-content v-if="loading">
+            <f7-navbar color="red" :title="groupData.groupName" back-link="Back">
+                <f7-nav-right>
+                    <f7-link icon-if-md="material:info" class="popover-open" href="#" data-popover=".popover-info">
+                    </f7-link><f7-link href="/home/" icon-if-md="material:home"></f7-link>
+                </f7-nav-right>
+            </f7-navbar>
+            <f7-list>
+                <f7-list-item>Group Name: {{groupData.groupName}}</f7-list-item>
+                <f7-list-item>Description: {{groupData.groupDescription}}</f7-list-item>
+                <f7-list-item media-item>Leader:
+                    <f7-chip :text="groupData.groupLeader.name" media-bg-color="orange">
+                        <f7-icon slot="media" material="person"></f7-icon>
+                    </f7-chip>
+                </f7-list-item>
+                <f7-list-item accordion-item title="Members">
+                    <f7-accordion-content>
+                        <f7-list-item v-for="(member, index) in groupData.groupMembers" :key="index">
+                            <f7-chip :text="member.name" media-bg-color="orange">
+                                <f7-icon slot="media" material="person"></f7-icon>
+                            </f7-chip>
+                        </f7-list-item>
+                    </f7-accordion-content>
+                </f7-list-item>
+                <f7-list-item title="Invite code:">
+                    <f7-badge style="font-size: 14px">{{groupId}}</f7-badge>
+                    <f7-button @click="copyToClipboard" icon-material="content_copy"></f7-button>
+                </f7-list-item>
+            </f7-list>
             <f7-block>
-                <p>Group Page</p>
-                <p>This is the group page. You can invite people into the group using by giving them the group's "Invite code" which they can in turn use it in the Enroll page's form.</p>
+                <f7-button color="red" class="col-80" fill @click="redirectTo">Schedule</f7-button>
+                <f7-button color="red" class="col-80" fill @click="openConfirmLeave(uid)">Leave Group</f7-button>
+                <f7-button color="red" class="col-80" fill @click="openConfirmDelete">Delete Group</f7-button>
             </f7-block>
-        </f7-popover>
-        <f7-input type="hidden" id="testing-code" v-bind:value="groupId"></f7-input>
+            <f7-popover class="popover-info">
+                <f7-block>
+                    <p>Group Page</p>
+                    <p>This is the group page. You can invite people into the group using by giving them the group's "Invite code" which they can in turn use it in the Enroll page's form.</p>
+                </f7-block>
+            </f7-popover>
+            <f7-input type="hidden" id="testing-code" v-bind:value="groupId"></f7-input>
+        </f7-page-content>
+
     </f7-page>
 </template>
 
@@ -50,7 +53,8 @@
             return {
                 uid : auth.currentUser.uid,
                 groupId: this.$f7route.params.groupId,
-                groupData: {}
+                groupData: {},
+                loading:false
             }
         },
         firebase() {
@@ -86,11 +90,15 @@
                 this.$f7router.navigate("/group/"+groupId+"/schedule/")
             },
             populateGroupData() {
-                this.$firebaseRefs.group.on("value", snapshot => {
+                this.$f7.dialog.preloader('Loading');
+                db.ref("groups").child(this.groupId).once("value", snapshot => {
                     this.groupData = snapshot.val()
                     console.log(this.groupData.groupLeader)
                     this.setGroupLeader(this.groupData.groupLeader)
                     this.setGroupMembers(snapshot.val().groupMembers)
+                }).then(()=>{
+                    this.loading=true;
+                    this.$f7.dialog.close()
                 })
             },
             showToastBottom() {
