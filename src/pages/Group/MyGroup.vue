bc84cdacbdc76dcbd63090d33558fa02a1a2f4ec
DESKTOP-3JDRVSO\User <chaninac127@gmail.com>
2018-06-07 16:18:02 +0700

    new card

--- src/pages/Group/MyGroup.vue
+++ src/pages/Group/MyGroup.vue
@@ -4,15 +4,35 @@
             <f7-navbar color="red" title="My Group" back-link="Back">
                 <f7-nav-right><f7-link href="/home/" icon-if-md="material:home"></f7-link></f7-nav-right>
             </f7-navbar>
-            <f7-list v-if="myGroups.length !== 0">
-                <f7-card v-for="group in myGroups" :key="group.groupId"
+            <!-- <f7-list v-if="myGroups.length !== 0">
+                <f7-card class="margin-vertical" v-for="group in myGroups" :key="group.groupId"
                          :title="group.groupName">
+                    <f7-content>
+
+                    </f7-content>
                     <f7-card-footer>
                         <span></span>
                         <f7-link @click="redirectTo(group.groupId)">See more</f7-link>
                     </f7-card-footer>
                 </f7-card>
-            </f7-list>
+            </f7-list> -->
+            <f7-card v-if="myGroups.length !== 0" class="margin-vertical" v-for="group in myGroups" :key="group.groupId">
+                <f7-icon size="30" slot="media" material="group"></f7-icon>
+                <f7-card-content>
+                    <f7-block>
+                        <f7-list>
+                            <f7-list-item>
+                                <f7-icon size="30" slot="media" material="group"></f7-icon>
+                                {{group.groupName}}
+                            </f7-list-item>
+                        </f7-list>
+                    </f7-block>
+                </f7-card-content>
+                <f7-card-footer>
+                    <span></span>
+                    <f7-link @click="redirectTo(group.groupId)">See more</f7-link>
+                </f7-card-footer>
+            </f7-card>
             <f7-block v-else strong>
                 <p>You haven't enrolled in any group yet.</p>
             </f7-block>
