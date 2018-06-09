671066487ce00a88567631b863e6c333d697fbc3
tui95 <khemmatat.thean@gmail.com>
2018-06-09 13:15:58 +0700

    fix condition rendering

--- src/pages/Group/MyGroup.vue
+++ src/pages/Group/MyGroup.vue
@@ -1,28 +1,30 @@
 <template>
     <f7-page>
-        <f7-navbar color="red" title="My Group" back-link="Back">
-            <f7-nav-right><f7-link href="/home/" icon-if-ios="f7:home" icon-if-md="material:home"></f7-link></f7-nav-right>
-        </f7-navbar>
-        <f7-card v-if="myGroups.length !== 0" class="margin-vertical" v-for="group in myGroups" :key="group.groupId">
-            <f7-card-content>
-                <f7-block>
-                    <f7-list>
-                        <f7-list-item :title="group.groupName">
-                            <div slot="media">
-                                <f7-icon size="30" material="group"></f7-icon>
-                            </div>
-                        </f7-list-item>
-                    </f7-list>
-                </f7-block>
-            </f7-card-content>
-            <f7-card-footer>
-                <span></span>
-                <f7-link @click="redirectTo(group.groupId)">See more</f7-link>
-            </f7-card-footer>
-        </f7-card>
-        <f7-block v-if="myGroups.length === 0" strong>
-            <b>You haven't enrolled in any group yet.</b>
-        </f7-block>
+        <f7-page v-if="loaded">
+            <f7-navbar color="red" title="My Group" back-link="Back">
+                <f7-nav-right><f7-link href="/home/" icon-if-ios="f7:home" icon-if-md="material:home"></f7-link></f7-nav-right>
+            </f7-navbar>
+            <f7-block v-if="myGroups.length === 0" strong>
+                <b>You haven't enrolled in any group yet.</b>
+            </f7-block>
+            <f7-card class="margin-vertical" v-for="group in myGroups" :key="group.groupId">
+                <f7-card-content>
+                    <f7-block>
+                        <f7-list>
+                            <f7-list-item :title="group.groupName">
+                                <div slot="media">
+                                    <f7-icon size="30" material="group"></f7-icon>
+                                </div>
+                            </f7-list-item>
+                        </f7-list>
+                    </f7-block>
+                </f7-card-content>
+                <f7-card-footer>
+                    <span></span>
+                    <f7-link @click="redirectTo(group.groupId)">See more</f7-link>
+                </f7-card-footer>
+            </f7-card>
+        </f7-page>
     </f7-page>
 </template>
 
